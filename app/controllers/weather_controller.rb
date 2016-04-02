9c3e7ec11365653330c2e427d8ca817be9e97579
Spencer Griffin <spencekarl@gmail.com>
2016-04-02 13:21:59 -0400

    added flickr image from location

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,14 +2,21 @@ class WeatherController < ApplicationController
   def index
     @latitude = request.location.latitude
     @longitude = request.location.longitude
+    # note: investigate whether there's a better way to access location.city
     location = Geocoder.search("#{@latitude},#{@longitude}").first
     forecast = ForecastIO.forecast(@latitude, @longitude)
 
+    # note: refactor everyhing and validate existence of large image
     @city = location.city
     @state = location.state
     @weather_icon = forecast.currently.icon
-    #binding.pry
+    photos = flickr.photos.search(:text => "#{@city} #{@state}", :sort => "relevance", :per_page => 5, :page => 1)
+    id     = photos[1].id
+    secret = photos[1].secret
+    info = flickr.photos.getInfo(:photo_id => id, :secret => secret)
+    sizes = flickr.photos.getSizes(:photo_id => id)
+    original = sizes.find {|s| s.label == 'Large' }
 
-    #@photos = flickr.photosForLocation(@latitude, @longitude, :accuracy => 6,:extras => 'extras',:per_page => 'per_page',:page => 'page')
+    @url = original.source
   end
 end
