7eec952a8ee815d7fca8f46c9df25cb16c396fd1
Spencer Griffin <spencekarl@gmail.com>
2016-04-02 08:16:09 -0400

    new rails app with base functionality to get lat/lon and show weather

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,12 @@
+class WeatherController < ApplicationController
+  def index
+
+    @latitude = request.location.latitude
+    @longitude = request.location.longitude
+
+    forecast = ForecastIO.forecast(@latitude, @longitude)
+    
+    @weather_icon = forecast.currently.icon
+    #binding.pry
+  end
+end
