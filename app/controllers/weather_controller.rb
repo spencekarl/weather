50a93a90f9641360b1f010bd00fbdc9f4518501f
Spencer Griffin <spencekarl@gmail.com>
2016-04-02 08:19:02 -0400

    changes js variable name

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,11 +1,10 @@
 class WeatherController < ApplicationController
   def index
-
     @latitude = request.location.latitude
     @longitude = request.location.longitude
 
     forecast = ForecastIO.forecast(@latitude, @longitude)
-    
+
     @weather_icon = forecast.currently.icon
     #binding.pry
   end
