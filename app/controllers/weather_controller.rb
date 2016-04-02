8d098e6df31ee15adf4f652d8eccf8dabb72f7a1
Spencer Griffin <spencekarl@gmail.com>
2016-04-02 11:27:35 -0400

    added city and state request

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -2,10 +2,14 @@ class WeatherController < ApplicationController
   def index
     @latitude = request.location.latitude
     @longitude = request.location.longitude
-
+    location = Geocoder.search("#{@latitude},#{@longitude}").first
     forecast = ForecastIO.forecast(@latitude, @longitude)
 
+    @city = location.city
+    @state = location.state
     @weather_icon = forecast.currently.icon
     #binding.pry
+
+    #@photos = flickr.photosForLocation(@latitude, @longitude, :accuracy => 6,:extras => 'extras',:per_page => 'per_page',:page => 'page')
   end
 end
