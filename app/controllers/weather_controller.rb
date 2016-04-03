d611692914acebd04ae08f54a718b2486dfb4438
Spencer Griffin <spencekarl@gmail.com>
2016-04-03 13:46:22 -0400

    refactoring and commenting

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,15 +1,33 @@
 class WeatherController < ApplicationController
   def index
+    # Stores all location and necessary weather data
     latitude, longitude = request.location.latitude, request.location.longitude
-
     location = Geocoder.search("#{latitude},#{longitude}").first
-    @city = location.city
-    @state = location.state
-
+    @city, @state = location.city, location.state
     forecast = ForecastIO.forecast(latitude, longitude)
-    @weather_icon = forecast.currently.icon
-    @temperature = forecast.currently.temperature.to_i
+    @weather_icon, @temperature = forecast.currently.icon, forecast.currently.temperature.to_i
 
+    # Search for photos at location
+    search_params = {:text => "#{@city} #{@state}", :sort => "relevance", :safe_search => 1, :content_type => 1,
+                     :per_page => 10, :page => 1, :tags => "get_tags", :extras => "url_k, url_h"}
+    photo_results = flickr.photos.search(search_params)
+
+    # Have to re-do search if no photo tags exist
+    if photo_results[0] == nil
+      search_params[:tags] = ""
+      photo_results = flickr.photos.search(search_params)
+    end
+
+    # Build list of potential photos to choose from and pick at random
+    url_list = []
+    photo_results.each { |photo| url_list.push(photo.url_k) if photo.respond_to?(:url_k) }
+    @location_photo = url_list[rand(url_list.length)]
+  end
+
+  private
+
+  # Build a string of photo tags based on the weather
+  def get_tags
     tags = "outdoor,outdoors,outside,landscape"
 
     case @weather_icon
@@ -24,27 +42,7 @@ class WeatherController < ApplicationController
     when "fog"
       tags.prepend("fog,foggy,")
     else
-      tags.prepend("sunny,")
+      tags.prepend("sun,sunny,")
     end
-
-    photo_results = flickr.photos.search(:text => "#{@city} #{@state}", :sort => "relevance",
-                                  :safe_search => 1, :content_type => 1, :per_page => 10, :page => 1, :tags => tags, :extras => "url_k, url_h")
-    if photo_results[0] == nil
-      photo_results = flickr.photos.search(:text => "#{@city} #{@state}", :sort => "relevance",
-                                    :safe_search => 1, :content_type => 1, :per_page => 10, :page => 1, :extras => "url_k, url_h")
-    end
-
-    url_list = []
-    photo_results.each { |photo| url_list.push(photo.url_k) if photo.respond_to?(:url_k) }
-
-    @location_photo = url_list[rand(url_list.length)]
-
-    # next up: full screen image, and stlye weather
-    # learn about scss
-    #          refactor, validate existence of large
-    #          refine search to get better results
-    #          deploy to heroku to test
-    #          figure out better way to geocode
-    #          rename variables and decide need for
   end
 end
